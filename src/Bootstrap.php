959d662e1b9383a365b11cc0b86a7f70581ceb96
“Risad212” <hafez.risad@gmail.com>
2026-06-07 14:45:06 +0600

    done

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -2,13 +2,21 @@
 /**
  * Bootstrap Whoops error handler.
  * Registers before everything else loads.
- * Catches all PHP errors and exceptions.
+ * Check Activate or not
  */
 
-if( get_option('devtrace_active') ){
-   $whoops = new \Whoops\Run();
-   $whoops->pushHandler( new \Whoops\Handler\PrettyPageHandler() );
+if( get_option('devtrace_active') ) {
+   $whoops  = new \Whoops\Run();
+   $handler = new \Whoops\Handler\PrettyPageHandler();
+
+   $handler->addDataTable( 'WordPress Context', [
+      'URL'          => $_SERVER['REQUEST_URI'] ?? '',
+      'WP Version'   => get_bloginfo( 'version' ),
+      'Active Theme' => wp_get_theme()->get( 'Name' ),
+      'Plugin Count' => count( get_option( 'active_plugins', [] ) ),
+   ] );
+
+   $whoops->pushHandler( $handler );
    $whoops->register();
 }
-
 
--- wp-devtrace.php
+++ wp-devtrace.php
@@ -80,3 +80,4 @@ final class WpDevTrace {
 }
 
 WpDevTrace::getInstance();
+
